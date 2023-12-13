24b744c69b9f33c155ece79a3062b8b63bbfc467
rlawlgp0197 <rlawlgp0197@gmail.com>
2023-12-13 11:51:43 +0900

    Initial Commit

---
+++ VoiceMemo/VoiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMemoApp.swift
+//  VoiceMemo
+//
+//  Created by KimJiHye on 2023/12/13.
+//
+// text용
+import SwiftUI
+
+@main
+struct VoiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
